5842e259db8fb65e29558bce69c3c31c2265a761
220403cs22cs12 <113499166+220403cs22cs12@users.noreply.github.com>
2022-09-15 16:13:19 +0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Lab1
+//
+//  Created by Azamat Islomov on 15/09/22.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    cout<<"azamat\n";
+    cout<<4+10<<endl;
+    return 0;
+}
